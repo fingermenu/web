d7f17880b0c6dca68ff4a0dc171fbc765ff8eea4
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-12-17 08:05:00 +1300

    Config files updated

--- src/framework/config/index.js
+++ src/framework/config/index.js
@@ -1,17 +1,17 @@
 // @flow
 
 const developmentConfig = {
-  parseServerUrl: 'https://trolleysmart-backend.herokuapp.com/parse/',
-  parseServerApplicationId: 'TrolleySmart',
+  parseServerUrl: 'https://fingermenu-backend.herokuapp.com/parse/',
+  parseServerApplicationId: 'FingerMenu',
   parseServerJavascriptKey: 'e74cce0d-e06c-4247-9628-945db4008d6f',
-  graphqlEndpoint: 'https://trolleysmart-backend.herokuapp.com/graphql',
+  graphqlEndpoint: 'https://fingermenu-backend.herokuapp.com/graphql',
 };
 
 const productionConfig = {
   parseServerUrl: 'https://parse.buddy.com/parse/',
-  parseServerApplicationId: '50a47f7f-411a-4abb-8c50-3daabac420eb',
-  parseServerJavascriptKey: 'w2GaCmTc2U7QwjbR3NGA1cg0UTjvbSYE',
-  graphqlEndpoint: 'https://50a47f7f-411a-4abb-8c50-3daabac420eb.parse.buddy.express/graphql',
+  parseServerApplicationId: '38066dde-ac12-4e7d-ac2b-ab03f3b6e56d',
+  parseServerJavascriptKey: '56LOhHqzPC1SRBtpa8vanDFCldkAVZjE',
+  graphqlEndpoint: 'https://38066dde-ac12-4e7d-ac2b-ab03f3b6e56d.parse.buddy.express/graphql',
 };
 
 export default (process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig);
